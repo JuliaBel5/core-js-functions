01374bbec6fe10c923f243d2f27fb44d77488995
JuliaBel5 <juliabel5@gmail.com>
2024-04-08 01:12:26 +0300

    feat: add 3 tasks solution

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -18,7 +18,7 @@
  *
  */
 function getCurrentFunctionName() {
-  throw new Error('Not implemented');
+  return getCurrentFunctionName.name;
 }
 
 /**
@@ -32,8 +32,12 @@ function getCurrentFunctionName() {
  *   getFunctionBody(hiHello) => "function hiHello() { console.log('hello world'); }"
  *
  */
-function getFunctionBody(/* func */) {
-  throw new Error('Not implemented');
+function getFunctionBody(func) {
+  if (typeof func !== 'function') {
+    return '';
+  }
+  const string = func.toString();
+  return string;
 }
 
 /**
@@ -50,8 +54,13 @@ function getFunctionBody(/* func */) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  const arr = [];
+
+  for (let i = 0; i < funcs.length; i += 1) {
+    arr.push(funcs[i].length);
+  }
+  return arr;
 }
 
 /**
