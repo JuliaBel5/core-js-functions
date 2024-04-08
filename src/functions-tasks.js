763b00931aa00c24d2202c77354aceb081adf238
JuliaBel5 <juliabel5@gmail.com>
2024-04-09 01:53:54 +0300

    feat: add 2 tasks solutions

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -144,8 +144,19 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  let counter = 0;
+  return function fu() {
+    try {
+      return func.call();
+    } catch (error) {
+      if (counter < attempts) {
+        counter += 1;
+        return fu();
+      }
+      throw new Error('test');
+    }
+  };
 }
 
 /**
@@ -171,8 +182,14 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function fu(...args) {
+    const newArgs = args.map((arg) => JSON.stringify(arg)).join(',');
+    logFunc(`${func.name}(${newArgs}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${newArgs}) ends`);
+    return result;
+  };
 }
 
 /**
